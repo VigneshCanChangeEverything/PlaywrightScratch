e3c993d72ba1336426678c25d08ed567c8f520e5
Vigneshwaran Subramanian <vigneshwaran.subramanian@esolutionsfirst.com>
2024-08-16 12:25:04 +0530

    Java Collections added

---
+++ src/test/java/org/java_practice/Arraylist.java
@@ -0,0 +1,65 @@
+
+package org.java_practice;
+
+import java.util.ArrayList;
+
+public class Arraylist {
+	
+	
+	public static void main (String [] args) {
+		
+	ArrayList <String> a = new ArrayList<>();
+		
+		a.add("Virtusa");
+		a.add("Esol");
+		for(int i = 0 ; i<a.size();i++) {
+			
+			System.out.println(a.get(i));
+			
+	
+		}			
+ArrayList <Integer> num = new ArrayList <Integer>();		
+		
+num.add(23);
+num.add(56);
+num.add(60);
+num.add(70);
+
+for (int j :num ) {
+	
+	System.out.println(j);
+	
+}
+		
+	}
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/test/java/org/java_practice/SortanArrayList.java
@@ -0,0 +1,62 @@
+package org.java_practice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortanArrayList {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> a = new ArrayList<String>();
+		
+		a.add("Ford Mustang");
+		a.add("Hundai");
+		a.add("AstonMartin");
+		a.add("Ferrari");
+		a.set(2, "Abrath");
+		
+		Collections.sort(a);
+		
+		for(String i : a ) {
+			
+			System.out.println(i);
+			
+		}
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
